76be75f818c13ef4f0127a567facbcec1d23a5d6
garganupam <103145642+garganupam@users.noreply.github.com>
2024-10-05 19:26:02 +0000

    Keywords

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144554
+let accountEmail="abc@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState
+  
+//accountId=2//Gives youu error as const values cannot be changed.
+
+
+accountEmail="abcd@gmail.com"
+accountPassword="3994"
+accountCity="Hamirpur"
+console.table([accountEmail,accountPassword,accountCity]);
+console.log(accountId)
