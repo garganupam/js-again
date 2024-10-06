aac6b8e3a2f3414460af79090068413a98179a4f
garganupam <103145642+garganupam@users.noreply.github.com>
2024-10-06 18:51:03 +0000

    operators

---
+++ 01_basics/04_operators.js
@@ -0,0 +1,24 @@
+let value=3
+let negValue=-value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1="hello";
+let str2=" hitesh"
+
+let str3=str1+str2;
+// console.log(str3)
+
+// console.log(1+"2"+3) //123
+// console.log(1+2+"3")//33
+// console.log("3"+3+3)//333
+
+// console.log(+true) //1
+// console.log(+"") //0
+
+//Strict check
+console.log("2"===2);  //it also checks the data type of both.
