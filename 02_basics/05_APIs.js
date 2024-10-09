cac89f6914a78baa88ba05f8d006633d5118584e
garganupam <103145642+garganupam@users.noreply.github.com>
2024-10-09 16:41:50 +0000

    APIs intro

---
+++ 02_basics/05_APIs.js
@@ -0,0 +1,13 @@
+//json is like an object whereas object has name but json has not
+// {
+//     "name":"hitesh",
+//     "coursename":"js",
+//     "price":"free"
+// }
+
+//You can get the api response as like an array
+[
+    {},
+    {},
+    {}
+]
